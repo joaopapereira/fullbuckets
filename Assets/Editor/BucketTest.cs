a8456c1b28d69498dedd7fb4c3bc2a7d10aab51a
Joao Pereira <joaopapereira@gmail.com>
2017-04-07 07:50:54 -0400

    Bucket Explosion and Reset

--- Assets/Editor/BucketTest.cs
+++ Assets/Editor/BucketTest.cs
@@ -13,7 +13,7 @@ public class BucketTest {
     public void AddOneDropToEmptyBucket_TotalDropsIs1()
     {
         bucket.AddDrop();
-        Assert.AreEqual(bucket.Size, 1);
+        Assert.AreEqual(1, bucket.Size);
         Assert.IsFalse(bucket.Full());
     }
 
@@ -25,8 +25,23 @@ public class BucketTest {
         bucket.AddDrop();
         bucket.AddDrop();
         bucket.AddDrop();
-        Assert.AreEqual(bucket.Size, 5);
+        Assert.AreEqual(5, bucket.Size);
         Assert.IsTrue(bucket.Full());
+    }
 
+    [Test]
+    public void AddSixDropToEmptyBucket_BucketExplodeCallFunctionAndResetsDropCount()
+    {
+        bool called = false;
+        bucket.SetExplosionCallback((Bucket bucket) => { called = true; return true; });
+        bucket.AddDrop();
+        bucket.AddDrop();
+        bucket.AddDrop();
+        bucket.AddDrop();
+        bucket.AddDrop();
+        bucket.AddDrop();
+        Assert.AreEqual(0, bucket.Size);
+        Assert.IsFalse(bucket.Full());
+        Assert.IsTrue(called);
     }
 }
--- Assets/scripts/GameLogic/Bucket.cs
+++ Assets/scripts/GameLogic/Bucket.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+delegate void ExplosionCallback(Bucket bucket);
 
 public class Bucket {
     private int maximumSize;
     private int currentSize;
+    private Func<Bucket, bool> explosionCallback;
     public int Size
     {
         get { return currentSize; }
@@ -10,17 +12,35 @@ public class Bucket {
 
     public Bucket(int maximumSize)
     {
-        currentSize = 0;
+        Reset();
         this.maximumSize = maximumSize;
     }
 
     public void AddDrop()
     {
-        currentSize++;
+        if (Full())
+        {
+            Reset();
+            explosionCallback(this);
+        }
+        else
+        {
+            currentSize++;
+        }
     }
 
     public bool Full()
     {
         return currentSize == maximumSize;
+    }
+
+    public void SetExplosionCallback(Func<Bucket, bool> explosionCallback)
+    {
+        this.explosionCallback = explosionCallback;
+    }
+
+    public void Reset(int currentSize = 0)
+    {
+        this.currentSize = currentSize;
     }
 }
